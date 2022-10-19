a3fc3fd036d818d2515770d886c288a0534e66b3
dygoode <dgoode92@yahoo.com>
2022-10-19 16:53:37 -0500

    W2D3 HW

---
+++ module.py
@@ -0,0 +1,18 @@
+#1 Has a function to calculate the area of a home
+ 
+from math import pi
+
+def area(length, width):
+    return length * width
+
+
+
+
+
+
+#2 Has a function to calculate the circumference of a circle
+
+def circumference():
+    radius = int(input("What is the radius of your circle?"))
+    circumference = radius * 2 * pi
+    return circumference
